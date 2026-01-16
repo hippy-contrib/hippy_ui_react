9437818118259e425ee08c0189fc0f708b16df93
jinzelin <jinzelin@tencent.com>
2026-01-16 18:46:38 +0800

    fix: 红点

--- packages/hippy_ui_react/src/components/Badge/renderInfo.tsx
+++ packages/hippy_ui_react/src/components/Badge/renderInfo.tsx
@@ -42,7 +42,7 @@ export default function getRenderInfo(params: BadgeRenderParams): BadgeRenderInf
     isShow = false;
   }
 
-  // 结果值
+  // 结果
   const result: BadgeRenderInfo = {
     badgeProps: {
       style: isShow
